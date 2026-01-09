94bc49f107b4637b87f63690bd3c8a7450a296b8
Woobeen Jeon <woobeen0516@gmail.com>
2026-01-10 08:32:30 +0900

    feat: async pathfinding

---
+++ chrona-server/src/main/java/org/wrd/chrona/async/entity/path/PathfindTaskRejectPolicy.java
@@ -0,0 +1,19 @@
+package org.wrd.chrona.async.entity.path;
+
+import net.minecraft.server.MinecraftServer;
+
+import java.util.Locale;
+
+public enum PathfindTaskRejectPolicy {
+    FLUSH_ALL,
+    CALLER_RUNS;
+
+    public static PathfindTaskRejectPolicy fromString(String policy) {
+        try {
+            return PathfindTaskRejectPolicy.valueOf(policy.toUpperCase(Locale.ROOT));
+        } catch (IllegalArgumentException e) {
+            MinecraftServer.LOGGER.warn("Invalid pathfind task reject policy: {}, falling back to {}.", policy, FLUSH_ALL.toString());
+            return FLUSH_ALL;
+        }
+    }
+}
--- chrona-server/src/main/java/org/wrd/chrona/configuration/ChronaConfiguration.java
+++ chrona-server/src/main/java/org/wrd/chrona/configuration/ChronaConfiguration.java
@@ -1,5 +1,7 @@
 package org.wrd.chrona.configuration;
 
+import org.wrd.chrona.async.entity.path.PathfindTaskRejectPolicy;
+
 import java.io.File;
 
 public final class ChronaConfiguration extends Configuration {
@@ -7,6 +9,13 @@ public final class ChronaConfiguration extends Configuration {
     private static final File CONFIG_FILE = new File("chrona.yml");
 
     public final Field<Boolean> minimizeTPSLag = createField("optimization.minimize-tps-lag", false);
+
+    public final Field<Boolean> optimizationAsyncPathfindingEnabled = createField("optimization.async.pathfinding.enabled", false);
+    public final Field<Integer> optimizationAsyncPathfindingMaxThreads = createField("optimization.async.pathfinding.max-threads", 0);
+    public final Field<Integer> optimizationAsyncPathfindingKeepalive = createField("optimization.async.pathfinding.keepalive", 60);
+    public final Field<Integer> optimizationAsyncPathfindingQueueSize = createField("optimization.async.pathfinding.queue-size", 0);
+    public final Field<String> optimizationAsyncPathfindingRejectPolicy = createField("optimization.async.pathfinding.reject-policy", PathfindTaskRejectPolicy.FLUSH_ALL.name());
+
     public final Field<Boolean> httpServerEnabled = createField("http-server.enabled", false);
     public final Field<Integer> httpServerPort = createField("http-server.port", 3000);
 
