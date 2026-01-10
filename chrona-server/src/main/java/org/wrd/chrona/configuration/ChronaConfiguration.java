e397bafac0718e8333acd0f39dd78527e4163adf
Woobeen Jeon <woobeen0516@gmail.com>
2026-01-10 15:31:18 +0900

    refactor: ChronaConfiguration

--- chrona-server/src/main/java/org/wrd/chrona/configuration/ChronaConfiguration.java
+++ chrona-server/src/main/java/org/wrd/chrona/configuration/ChronaConfiguration.java
@@ -8,7 +8,7 @@ public final class ChronaConfiguration extends Configuration {
     private static final int CONFIG_VERSION = 100;
     private static final File CONFIG_FILE = new File("chrona.yml");
 
-    public final Field<Boolean> minimizeTPSLag = createField("optimization.minimize-tps-lag", false);
+    public final Field<Boolean> optimizationMinimizeTPSLag = createField("optimization.minimize-tps-lag", false);
 
     public final Field<Boolean> optimizationAsyncPathfindingEnabled = createField("optimization.async.pathfinding.enabled", false);
     public final Field<Integer> optimizationAsyncPathfindingMaxThreads = createField("optimization.async.pathfinding.max-threads", 0);
