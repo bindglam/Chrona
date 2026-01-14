cfe22f0ae51377ba8065a0c707769528216cc3e7
bindglam <woobeen0516@gmail.com>
2026-01-14 21:20:55 +0900

    fix: Minimize-TPS-lag.patch

--- chrona-server/src/main/java/org/wrd/chrona/async/entity/path/AsyncPathProcessor.java
+++ chrona-server/src/main/java/org/wrd/chrona/async/entity/path/AsyncPathProcessor.java
@@ -1,7 +1,7 @@
 package org.wrd.chrona.async.entity.path;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
-import net.minecraft.Util;
+import net.minecraft.util.Util;
 import net.minecraft.world.level.pathfinder.Path;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
