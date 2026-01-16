c45c281a52f953148c3b82ab3042cf57e6bfb2fb
bindglam <woobeen0516@gmail.com>
2026-01-16 15:06:54 +0900

    refactor: remove http api

--- chrona-server/src/main/java/org/wrd/chrona/http/api/ApiHandler.java
+++
@@ -1,9 +0,0 @@
-package org.wrd.chrona.http.api;
-
-public interface ApiHandler {
-    default void get(HandlerContext context) {
-    }
-
-    default void post(HandlerContext context) {
-    }
-}
--- chrona-server/src/main/java/org/wrd/chrona/http/api/RequestMethod.java
+++
@@ -1,6 +0,0 @@
-package org.wrd.chrona.http.api;
-
-public enum RequestMethod {
-    GET,
-    POST
-}
